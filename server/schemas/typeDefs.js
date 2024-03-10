5e5bdf9d4536388f3066229ddcece75e074c5eec
unknown <anthony_rosati@yahoo.com>
2024-03-10 16:31:45 -0500

    Added fantasy location to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,15 +25,27 @@ const typeDefs = `
     tags: [String!]
   }
 
+  # Type for fantasy locations
+  type FantasyLocation {
+    name: String!
+    locationLink: Location
+  }
+
   type Query {
     locations: [Location]
     locationsByTags(tags: TagInput!): [Location]
+    fantasyLocations: [FantasyLocation]
+    fantasyLocationByName(name: String!): FantasyLocation
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
 
     addUser(email: String!, password: String!): Auth
+
+    addFantasyLocation(name: String!, locationLink: Location): FantasyLocation
+
+    editFantasyLocation(name: String, locationLink: Location): FantasyLocation
   }
 `
 
