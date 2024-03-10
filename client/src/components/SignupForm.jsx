1be565e19c28338f7a67354b46270c733127a7fc
unknown <anthony_rosati@yahoo.com>
2024-03-09 23:45:54 -0600

    Auth added using JWT

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { validateEmail } from '../utils/validateEmail';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignupForm() {
     const [email, setEmail] = useState('');
---
+++ client/src/utils/auth.js
@@ -0,0 +1,47 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+    getUser() {
+        return decode(this.getToken());
+    };
+
+    // Checks if user is still logged in
+    loggedIn() {
+        // Checks for saved token and whether it's still valid
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    };
+
+    // Check for expired token
+    isTokenExpired(token) {
+        try {
+            // Decodes token
+            const decoded = decode(token);
+            // Checks for time remaining before token expiration
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            return false;
+        };
+    };
+
+    // For getting the user token from localStorage
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
+
+    // For setting a user token on login in localStorage
+    login(idToken) {
+        // Save token to localStorage
+        localStorage.setItem('id_token', idToken);
+    };
+
+    // For removing user token from localStorage on user logout
+    logout() {
+        // Removes token from localStorage
+        localStorage.removeItem('id_token')
+    };
+};
+
+export default new AuthService();
