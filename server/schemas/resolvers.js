21ad2e717aa4798def54a150a2e1f2e982ee8701
unknown <anthony_rosati@yahoo.com>
2024-03-10 00:29:32 -0600

    Login and addUser resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,47 @@
 const { signToken, AuthenticationError } = require('../utils/auth');
+const { User, Location } = require('../models');
+
+const resolvers = {
+  Query: {
+
+  },
+
+  Mutation: {
+    addUser: async (parent, { userData }) => {
+      try {
+        const user = await User.create({ userData });
+        const token = signToken(user);
+        return { token, user };
+      } catch (err) {
+        console.error('Error creating user: ', err);
+        throw new Error('Could not create user.');
+      }
+    },
+
+    login: async (parent, { email, password }) => {
+      try {
+
+        // Checks for valid user
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw AuthenticationError;
+        };
+
+        // Checks for valid password
+        const validPassword = await user.isValidPassword(password);
+        if (!validPassword) {
+          throw AuthenticationError;
+        };
+
+        const token = signToken(user);
+        return { token, user }
+
+      } catch (err) {
+        console.error('Error logging in :', err);
+        throw new Error('Could not log in.');
+      }
+    }
+  }
+};
+
+module.exports = resolvers;
