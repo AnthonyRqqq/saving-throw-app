35337cdfb56ddd756e6b98e6216853732263e9ec
unknown <anthony_rosati@yahoo.com>
2024-03-10 21:17:04 -0500

    Queries in client folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client';
+
+// Executing getting all locations
+export const GET_LOCATIONS = gql`
+    query locations{
+        location{
+            _id
+            lat
+            lon
+            tags
+        }
+    }
+`
+
+// Executing getting locations based on provided tags
+export const GET_LOCATIONS_BY_TAGS = gql`
+    query locationsByTags($tags: [String!]) {
+        location(tags: $tags) {
+            _id
+            lat
+            lon
+            tags
+        }
+    }
+`
+
+// Executing getting all fantasy locations
+export const GET_FANTASY_LOCATIONS = gql`
+    query fantasyLocations {
+        fantasylocation {
+            _id
+            name
+            realLocation {
+                _id
+                lat
+                lon
+                tags
+            }
+        }
+    }
+`
+
+// Executing getting fantasy location by name
+export const GET_FANTASY_LOCATION_BY_NAME = gql`
+    query fantasyLocationByName($name: String!) {
+        fantasylocation(name: $name) {
+            _id
+            name
+            realLocation {
+                _id
+                lat
+                lon
+                tags
+            }
+        }
+    }
+`
