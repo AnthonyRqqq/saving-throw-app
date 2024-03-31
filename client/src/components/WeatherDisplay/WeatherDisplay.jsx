933519db43ad4b1bde018cd42ba94ba080656526
unknown <anthony_rosati@yahoo.com>
2024-03-31 17:28:34 -0500

    Hover css applied to fantasy location names

--- client/src/components/WeatherDisplay/WeatherDisplay.jsx
+++ client/src/components/WeatherDisplay/WeatherDisplay.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client";
 import { GET_FANTASY_LOCATIONS } from "../../utils/queries";
 import { useEffect, useState } from "react";
 import { weatherSearch } from "../../utils/weatherSearch";
+import './WeatherDisplay.css'
 
 export default function WeatherDisplayComponent() {
 
@@ -10,15 +11,12 @@ export default function WeatherDisplayComponent() {
     const { loading, data } = useQuery(GET_FANTASY_LOCATIONS);
 
     useEffect(() => {
-
-        console.log(data)
         if (data && !loading) {
+            // Gets list of fantasy locations and sets the state
+            console.log(data)
             const fantasyLocationData = data.fantasyLocations;
             setFantasyLocations(fantasyLocationData);
             console.log(fantasyLocations)
-
-            // weatherSearch(lat, lon);
-
         }
     }, [data, loading])
 
@@ -27,12 +25,14 @@ export default function WeatherDisplayComponent() {
         <div>
             {data && (
                 <div>
+                    {/* Iterates through fantasy locations, displays data and assigns lat and lon individually */}
                     {fantasyLocations.map(location => (
                         <div key={location._id}>
                             <span
                                 onClick={(event) => weatherSearch(parseFloat(event.target.dataset.lat), parseFloat(event.target.dataset.lon))}
                                 data-lat={location.realLocation.lat}
                                 data-lon={location.realLocation.lon}
+                                className="fantasyLocationName"
                             >{location.name}
                             </span>
                             <span>{location.realLocation.name}</span>
