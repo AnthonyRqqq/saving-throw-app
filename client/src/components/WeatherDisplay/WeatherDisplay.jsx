dc6e4dd2050dccd8ff4efbd5bc6b6d1dab2e99cc
unknown <anthony_rosati@yahoo.com>
2024-04-06 21:02:17 -0500

    Reorganized code via prettier and jslint

--- client/src/components/WeatherDisplay/WeatherDisplay.jsx
+++ client/src/components/WeatherDisplay/WeatherDisplay.jsx
@@ -2,114 +2,127 @@ import { useQuery } from "@apollo/client";
 import { GET_FANTASY_LOCATIONS } from "../../utils/queries";
 import { useEffect, useState } from "react";
 import { weatherSearch } from "../../utils/weatherSearch";
-import './WeatherDisplay.css'
+import "./WeatherDisplay.css";
 
 export default function WeatherDisplayComponent() {
+  const [fantasyLocations, setFantasyLocations] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+  const [weatherState, setWeatherState] = useState(false);
+  const [seeMore, setSeeMore] = useState(false);
+  const [currentFantastyLocation, setCurrentFantasyLocation] = useState("");
+  // const [currentRealLocation, setCurrentRealLocation] = useState('');
 
-    const [fantasyLocations, setFantasyLocations] = useState([]);
-    const [weatherData, setWeatherData] = useState([]);
-    const [weatherState, setWeatherState] = useState(false);
-    const [seeMore, setSeeMore] = useState(false);
-    const [currentFantastyLocation, setCurrentFantasyLocation] = useState('');
-    // const [currentRealLocation, setCurrentRealLocation] = useState('');
+  const { loading, data } = useQuery(GET_FANTASY_LOCATIONS);
 
-    const { loading, data } = useQuery(GET_FANTASY_LOCATIONS);
-
-    useEffect(() => {
-        if (data && !loading) {
-            // Gets list of fantasy locations and sets the state
-            console.log(data)
-            const fantasyLocationData = data.fantasyLocations;
-            setFantasyLocations(fantasyLocationData);
-            console.log(fantasyLocations)
-        }
-    }, [data, loading])
-
-    const handleWeatherSearch = async (e) => {
-        const lat = parseFloat(e.target.dataset.lat);
-        const lon = parseFloat(e.target.dataset.lon);
-
-        const weatherResult = await weatherSearch(lat, lon);
-        console.log(weatherResult)
-        // setCurrentFantasyLocation(e.target.dataset.fantasyname);
-        // setCurrentRealLocation(e.target.dataset.realname);
-        await setWeatherData(weatherResult);
-        await setCurrentFantasyLocation(e.target.dataset.fantasyname)
-        // Resets weather state to true or initializes it if false
-        if (weatherState) {
-            setWeatherState(false);
-            setWeatherState(true);
-        } else {
-            setWeatherState(true);
-        }
+  useEffect(() => {
+    if (data && !loading) {
+      // Gets list of fantasy locations and sets the state
+      console.log(data);
+      const fantasyLocationData = data.fantasyLocations;
+      setFantasyLocations(fantasyLocationData);
+      console.log(fantasyLocations);
     }
+  }, [data, loading]);
 
-    // For clicking the 'see more' button, gives further details of weather
-    const handleSeeMore = async () => {
-        await setSeeMore(true);
+  const handleWeatherSearch = async (e) => {
+    const lat = parseFloat(e.target.dataset.lat);
+    const lon = parseFloat(e.target.dataset.lon);
+
+    const weatherResult = await weatherSearch(lat, lon);
+    console.log(weatherResult);
+    // setCurrentFantasyLocation(e.target.dataset.fantasyname);
+    // setCurrentRealLocation(e.target.dataset.realname);
+    await setWeatherData(weatherResult);
+    await setCurrentFantasyLocation(e.target.dataset.fantasyname);
+    // Resets weather state to true or initializes it if false
+    if (weatherState) {
+      setWeatherState(false);
+      setWeatherState(true);
+    } else {
+      setWeatherState(true);
     }
+  };
 
-    // For clicking the 'see less' button, hides potentially less useful details
-    const handleCollapseSeeMore = async () => {
-        await setSeeMore(false);
-    }
+  // For clicking the 'see more' button, gives further details of weather
+  const handleSeeMore = async () => {
+    await setSeeMore(true);
+  };
 
+  // For clicking the 'see less' button, hides potentially less useful details
+  const handleCollapseSeeMore = async () => {
+    await setSeeMore(false);
+  };
 
-    return (
-        <div>
-            {weatherState && (
-                <div className="weatherDisplay">
-                    <span>{currentFantastyLocation}</span>
-                    <span>Current Temp: {weatherData.main.temp}</span>
-                    <span>Weather: {weatherData.weather[0].main} <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}></img></span>
-                    {/* For seeing further details about weather, switches to hide to get rid of details */}
-                    <span>
-                        {seeMore ? (
-                            <div className="clickText" onClick={() => handleCollapseSeeMore()}>
-                                Hide
-                            </div>
-                        ) : (
-                            <div className="clickText" onClick={() => handleSeeMore()}>
-                                See More
-                            </div>
-                        )}
-                        {seeMore && (
-                            <ul>
-                                <li>Feels Like: {weatherData.main.feels_like}</li>
-                                <li>Visibility: {weatherData.visibility / 1000} km</li>
-                                <li>Humidity: {weatherData.main.humidity}%</li>
-                                <li>Wind Speed: {weatherData.wind.speed} mph</li>
-                                <li>Cloud Cover: {weatherData.weather.clouds}%</li>
-                                <li><img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}></img></li>
-                            </ul>
-                        )}
-                    </span>
-                </div>
+  return (
+    <div>
+      {weatherState && (
+        <div className="weatherDisplay">
+          <span>{currentFantastyLocation}</span>
+          <span>Current Temp: {weatherData.main.temp}</span>
+          <span>
+            Weather: {weatherData.weather[0].main}{" "}
+            <img
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+            ></img>
+          </span>
+          {/* For seeing further details about weather, switches to hide to get rid of details */}
+          <span>
+            {seeMore ? (
+              <div
+                className="clickText"
+                onClick={() => handleCollapseSeeMore()}
+              >
+                Hide
+              </div>
+            ) : (
+              <div className="clickText" onClick={() => handleSeeMore()}>
+                See More
+              </div>
             )}
-
-            {data && (
-                <div className="row fantasyLocationItem">
-                    {/* Iterates through fantasy locations, displays data and assigns lat and lon individually */}
-                    {fantasyLocations.map(location => (
-                        <div className="col-4" key={location._id}>
-                            <div className="col">
-                                <span
-                                    onClick={(e) => handleWeatherSearch(e)}
-                                    data-lat={location.realLocation.lat}
-                                    data-lon={location.realLocation.lon}
-                                    data-fantasyname={location.name}
-                                    data-realname={location.realLocation.name}
-                                    className="fantasyLocationName"
-                                >{location.name}
-                                </span>
-                            </div>
-                            <div className="col">
-                                <span className="realLocationName">({location.realLocation.name})</span>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+            {seeMore && (
+              <ul>
+                <li>Feels Like: {weatherData.main.feels_like}</li>
+                <li>Visibility: {weatherData.visibility / 1000} km</li>
+                <li>Humidity: {weatherData.main.humidity}%</li>
+                <li>Wind Speed: {weatherData.wind.speed} mph</li>
+                <li>Cloud Cover: {weatherData.weather.clouds}%</li>
+                <li>
+                  <img
+                    src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                  ></img>
+                </li>
+              </ul>
             )}
+          </span>
         </div>
-    );
+      )}
+
+      {data && (
+        <div className="row fantasyLocationItem">
+          {/* Iterates through fantasy locations, displays data and assigns lat and lon individually */}
+          {fantasyLocations.map((location) => (
+            <div className="col-4" key={location._id}>
+              <div className="col">
+                <span
+                  onClick={(e) => handleWeatherSearch(e)}
+                  data-lat={location.realLocation.lat}
+                  data-lon={location.realLocation.lon}
+                  data-fantasyname={location.name}
+                  data-realname={location.realLocation.name}
+                  className="fantasyLocationName"
+                >
+                  {location.name}
+                </span>
+              </div>
+              <div className="col">
+                <span className="realLocationName">
+                  ({location.realLocation.name})
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
--- client/src/components/WeatherSearchForm/WeatherSearchForm.jsx
+++ client/src/components/WeatherSearchForm/WeatherSearchForm.jsx
@@ -1,243 +1,298 @@
-import { useState, useEffect } from 'react';
-import { useMutation } from '@apollo/client';
-import { useQuery } from '@apollo/client';
-import { GET_LOCATIONS } from '../../utils/queries';
-import { CREATE_FANTASY_LOCATION } from '../../utils/mutations';
-import './WeatherSearchForm.css'
+import { useState, useEffect } from "react";
+import { useMutation } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { GET_LOCATIONS } from "../../utils/queries";
+import { CREATE_FANTASY_LOCATION } from "../../utils/mutations";
+import "./WeatherSearchForm.css";
 
 export default function WeatherSearchForm() {
+  const [tags, setTags] = useState([]);
+  const [selectedTag, setSelectedTag] = useState("");
+  const [tagLimit, setTagLimit] = useState(false);
+  const [filteredLocations, setFilteredLocations] = useState([]);
+  const [fantasyLocationName, setFantasyLocationName] = useState("");
+  const [allLocations, setAllLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [selectedLocationName, setSelectedLocationName] = useState("");
 
-    const [tags, setTags] = useState([]);
-    const [selectedTag, setSelectedTag] = useState('');
-    const [tagLimit, setTagLimit] = useState(false);
-    const [filteredLocations, setFilteredLocations] = useState([]);
-    const [fantasyLocationName, setFantasyLocationName] = useState('');
-    const [allLocations, setAllLocations] = useState([]);
-    const [selectedLocation, setSelectedLocation] = useState('')
-    const [selectedLocationName, setSelectedLocationName] = useState('')
+  const tagOptions = [
+    "Desert",
+    "Dunes",
+    "Hot",
+    "Arid",
+    "Cold",
+    "Tundra",
+    "Windy",
+    "Snowy",
+    "Tropical",
+    "Jungle",
+    "River",
+    "Rainy",
+    "Warm",
+    "Moderate",
+    "Coastal",
+    "Mountains",
+    "Marshes",
+    "Forest",
+    "Windy",
+    "Stormy",
+    "Plains",
+    "River",
+    "Dry",
+  ];
 
-    const tagOptions = ['Desert', 'Dunes', 'Hot', 'Arid', 'Cold', 'Tundra', 'Windy', 'Snowy', 'Tropical', 'Jungle', 'River', 'Rainy', 'Warm', 'Moderate', 'Coastal', 'Mountains', 'Marshes', 'Forest', 'Windy', 'Stormy', 'Plains', 'River', 'Dry']
+  // // Define queries
+  const { loading: allLocationLoading, data: allLocationData } =
+    useQuery(GET_LOCATIONS);
 
-    // // Define queries
-    const { loading: allLocationLoading, data: allLocationData } = useQuery(GET_LOCATIONS);
+  // Define mutation
+  const [createFantasyLocation] = useMutation(CREATE_FANTASY_LOCATION);
 
-    // Define mutation
-    const [createFantasyLocation] = useMutation(CREATE_FANTASY_LOCATION);
-
-    useEffect(() => {
-        // Defines location data when loaded
-        if (!allLocationLoading && allLocationData) {
-            const locations = allLocationData.locations;
-            setAllLocations(locations);
-        }
-
-        // Resets filtered locations when tags are selected or deleted
-        if (tags) {
-            locationFilter();
-        }
-
-        if (fantasyLocationName && selectedLocation) {
-            console.log(fantasyLocationName, selectedLocation)
-        }
-
-    }, [allLocationData, allLocationLoading, tags, fantasyLocationName, selectedLocation])
-
-    const handleFantasyLocationCreation = async () => {
-
-        if (fantasyLocationName === '') {
-            return;
-        };
-
-        try {
-            const response = await createFantasyLocation({
-                variables: {
-                    name: fantasyLocationName,
-                    realLocation: selectedLocation
-                }
-            });
-
-            console.log('Fantasy location created: ', response.data);
-        } catch (err) {
-            console.error('Error creating fantasy location: ', err)
-        }
+  useEffect(() => {
+    // Defines location data when loaded
+    if (!allLocationLoading && allLocationData) {
+      const locations = allLocationData.locations;
+      setAllLocations(locations);
     }
 
-    // For setting up list of tags to filter locations by
-    const handleTagSelect = async () => {
-
-        // Keeps duplicate tags from being added
-        if (tags.includes(selectedTag)) {
-            return;
-        }
-
-        // Keeps search results limited to three, sets tag limit to display error message
-        if (tags.length === 3) {
-            setTagLimit(true);
-            return;
-        }
-
-        // Get the name of the tag and adds it to the tags array
-        const newTagsArray = [...tags, selectedTag];
-
-        // Updates state
-        setTags(newTagsArray);
-    };
-
-    // For deleting tags from list
-    const handleDeleteTag = async (index) => {
-        const updatedTags = [...tags];
-        updatedTags.splice(index, 1);
-        setTags(updatedTags);
-        
-
-        // Resets tag limit to erase error message
-        if (tagLimit) {
-            setTagLimit(false);
-        };
-
-        handleLocationSelect();
+    // Resets filtered locations when tags are selected or deleted
+    if (tags) {
+      locationFilter();
     }
 
-    const handleInputChange = async (e) => {
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+    if (fantasyLocationName && selectedLocation) {
+      console.log(fantasyLocationName, selectedLocation);
+    }
+  }, [
+    allLocationData,
+    allLocationLoading,
+    tags,
+    fantasyLocationName,
+    selectedLocation,
+  ]);
 
-        if (inputType === 'fantasyLocationName') {
-            setFantasyLocationName(inputValue);
-        } else if (inputType === 'tagSelect') {
-            setSelectedTag(inputValue);
-        }
-    };
-
-    const handleFormSubmit = async (e) => {
-
-        // Prevents page refresh
-        e.preventDefault();
-
-        try {
-
-        } catch (err) {
-            console.error(err);
-            throw new Error('Could not create linked location.')
-        }
-    };
-
-    // Filters locations based on selected tags at tag change
-    const locationFilter = async () => {
-        if (allLocations) {
-            const matchingLocations = [];
-
-            // Convert tags to lowercase
-            const lowercaseTags = tags.map(tag => tag.toLowerCase());
-
-            for (const location of allLocations) {
-                // Convert location tags to lowercase
-                const lowercaseLocationTags = location.tags.map(tag => tag.toLowerCase());
-
-                // Check if all lowercase tags are included in lowercase location tags
-                if (lowercaseTags.every(tag => lowercaseLocationTags.includes(tag))) {
-                    matchingLocations.push(location);
-                }
-            };
-
-            // Update filteredLocations with new matching locations
-            setFilteredLocations(matchingLocations);
-        }
-    };
-
-    const handleLocationSelect = async (e) => {
-        if (e === undefined) {
-            setSelectedLocation('');
-            setSelectedLocationName('');
-            return;
-        }
-        const { target } = e;
-        setSelectedLocation(target.value);
-        setSelectedLocationName(target.textContent);
-
+  const handleFantasyLocationCreation = async () => {
+    if (fantasyLocationName === "") {
+      return;
     }
 
-    return (
-        <div className='form-div'>
-            <h3 className='row justify-content-center'>Weather Search</h3>
-            <form className='weather-search-form justify-content-center' onSubmit={handleFormSubmit}>
+    try {
+      const response = await createFantasyLocation({
+        variables: {
+          name: fantasyLocationName,
+          realLocation: selectedLocation,
+        },
+      });
 
-                {/* Input field for the name of the fantasy location */}
-                <div className='row justify-content-center'>
-                    <input
-                        className='col-3'
-                        value={fantasyLocationName}
-                        name='fantasyLocationName'
-                        onChange={handleInputChange}
-                        type='text'
-                        placeholder='Fantasy Location Name'
-                    />
-                </div>
+      console.log("Fantasy location created: ", response.data);
+    } catch (err) {
+      console.error("Error creating fantasy location: ", err);
+    }
+  };
 
-                {/* For selecting tags */}
-                <div className='row justify-content-center'>
-                    {/* Div to separate span onto own line */}
-                    <div className='row justify-content-center'>
-                        <p className='row justify-content-center mt-2'>Select your tags. (Max of 3)</p>
-                    </div>
+  // For setting up list of tags to filter locations by
+  const handleTagSelect = async () => {
+    // Keeps duplicate tags from being added
+    if (tags.includes(selectedTag)) {
+      return;
+    }
 
-                    {/* Dropdown of tag options */}
-                    <select className='col-1' name='tagSelect' onChange={handleInputChange}>
-                        <option value='' disabled selected style={{ textAlign: 'center' }}></option>
-                        {tagOptions.map((tag, index) => (
-                            <option className='tagSelect' key={index} value={tag} style={{ textAlign: 'center' }}>{tag}</option>
-                        ))};
-                    </select>
+    // Keeps search results limited to three, sets tag limit to display error message
+    if (tags.length === 3) {
+      setTagLimit(true);
+      return;
+    }
 
-                    {/* Button for adding tags to the search array */}
-                    <div className='row justify-content-center'>
-                        <button className='col-1 mt-2 tagBtn' onClick={handleTagSelect}>Add Tag</button>
-                        {/* Displays error when too many tags are selected */}
-                        {tagLimit && (
-                            <p className='row justify-content-center'>No more than three tags possible.</p>
-                        )}
-                    </div>
+    // Get the name of the tag and adds it to the tags array
+    const newTagsArray = [...tags, selectedTag];
 
-                    {/* Displays selected tags, max of three */}
-                    <div className='container'>
-                        <div className='row justify-content-center'>
-                            <ul className='list-unstyled d-flex justify-content-center'>
-                                {tags.map((tag, index) => (
-                                    <li key={index} className='mx-2'>
-                                        <button onClick={() => handleDeleteTag(index)}>{tag}</button>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+    // Updates state
+    setTags(newTagsArray);
+  };
 
-                <div className='container'>
-                    <div className='row justify-content-center'>
-                        <ul className='list-unstyled d-flex justify-content-center'>
-                            {filteredLocations.length !== allLocations.length && (
-                                filteredLocations.map((location, index) => {
-                                    return (
-                                        <li className='mx-2' key={index}><button className='locationBtn' onClick={handleLocationSelect} value={location._id}>{location.name}</button></li>
-                                    )
-                                })
-                            )}
-                        </ul>
-                    </div>
-                </div>
+  // For deleting tags from list
+  const handleDeleteTag = async (index) => {
+    const updatedTags = [...tags];
+    updatedTags.splice(index, 1);
+    setTags(updatedTags);
 
-                {selectedLocation && (
-                    <div className='row justify-content-center'>
-                        <p className='row justify-content-center'>Selected Location: {selectedLocationName}</p>
-                    </div>
-                )}
+    // Resets tag limit to erase error message
+    if (tagLimit) {
+      setTagLimit(false);
+    }
 
+    handleLocationSelect();
+  };
 
-                <div className='row justify-content-center'>
-                    <button className='row justify-content-center col-1' onClick={handleFantasyLocationCreation}>Submit</button>
-                </div>
-            </form>
+  const handleInputChange = async (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "fantasyLocationName") {
+      setFantasyLocationName(inputValue);
+    } else if (inputType === "tagSelect") {
+      setSelectedTag(inputValue);
+    }
+  };
+
+  const handleFormSubmit = async (e) => {
+    // Prevents page refresh
+    e.preventDefault();
+  };
+
+  // Filters locations based on selected tags at tag change
+  const locationFilter = async () => {
+    if (allLocations) {
+      const matchingLocations = [];
+
+      // Convert tags to lowercase
+      const lowercaseTags = tags.map((tag) => tag.toLowerCase());
+
+      for (const location of allLocations) {
+        // Convert location tags to lowercase
+        const lowercaseLocationTags = location.tags.map((tag) =>
+          tag.toLowerCase()
+        );
+
+        // Check if all lowercase tags are included in lowercase location tags
+        if (lowercaseTags.every((tag) => lowercaseLocationTags.includes(tag))) {
+          matchingLocations.push(location);
+        }
+      }
+
+      // Update filteredLocations with new matching locations
+      setFilteredLocations(matchingLocations);
+    }
+  };
+
+  const handleLocationSelect = async (e) => {
+    if (e === undefined) {
+      setSelectedLocation("");
+      setSelectedLocationName("");
+      return;
+    }
+    const { target } = e;
+    setSelectedLocation(target.value);
+    setSelectedLocationName(target.textContent);
+  };
+
+  return (
+    <div className="form-div">
+      <h3 className="row justify-content-center">Weather Search</h3>
+      <form
+        className="weather-search-form justify-content-center"
+        onSubmit={handleFormSubmit}
+      >
+        {/* Input field for the name of the fantasy location */}
+        <div className="row justify-content-center">
+          <input
+            className="col-3"
+            value={fantasyLocationName}
+            name="fantasyLocationName"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Fantasy Location Name"
+          />
         </div>
-    )
-};
+
+        {/* For selecting tags */}
+        <div className="row justify-content-center">
+          {/* Div to separate span onto own line */}
+          <div className="row justify-content-center">
+            <p className="row justify-content-center mt-2">
+              Select your tags. (Max of 3)
+            </p>
+          </div>
+
+          {/* Dropdown of tag options */}
+          <select className="col-1" name="tagSelect">
+            <option
+              value=""
+              disabled
+              selected
+              style={{ textAlign: "center" }}
+            ></option>
+            {tagOptions.map((tag, index) => (
+              <option
+                className="tagSelect"
+                key={index}
+                value={tag}
+                style={{ textAlign: "center" }}
+              >
+                {tag}
+              </option>
+            ))}
+            ;
+          </select>
+
+          {/* Button for adding tags to the search array */}
+          <div className="row justify-content-center">
+            <button className="col-1 mt-2 tagBtn" onClick={handleTagSelect}>
+              Add Tag
+            </button>
+            {/* Displays error when too many tags are selected */}
+            {tagLimit && (
+              <p className="row justify-content-center">
+                No more than three tags possible.
+              </p>
+            )}
+          </div>
+
+          {/* Displays selected tags, max of three */}
+          <div className="container">
+            <div className="row justify-content-center">
+              <ul className="list-unstyled d-flex justify-content-center">
+                {tags.map((tag, index) => (
+                  <li key={index} className="mx-2">
+                    <button onClick={() => handleDeleteTag(index)}>
+                      {tag}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row justify-content-center">
+            <ul className="list-unstyled d-flex justify-content-center">
+              {filteredLocations.length !== allLocations.length &&
+                filteredLocations.map((location, index) => {
+                  return (
+                    <li className="mx-2" key={index}>
+                      <button
+                        className="locationBtn"
+                        onClick={handleLocationSelect}
+                        value={location._id}
+                      >
+                        {location.name}
+                      </button>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
+        </div>
+
+        {selectedLocation && (
+          <div className="row justify-content-center">
+            <p className="row justify-content-center">
+              Selected Location: {selectedLocationName}
+            </p>
+          </div>
+        )}
+
+        <div className="row justify-content-center">
+          <button
+            className="row justify-content-center col-1"
+            onClick={handleFantasyLocationCreation}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
