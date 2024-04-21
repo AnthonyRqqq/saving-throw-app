e189bb36ed791f1a31a029efc2d56370f556ff88
unknown <anthony_rosati@yahoo.com>
2024-04-20 20:14:14 -0500

    Reorganize server

--- server/server.js
+++ server/server.js
@@ -1,53 +1,53 @@
-const express = require('express');
-const cors = require('cors')
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
 const startApolloServer = async () => {
-    await server.start();
+  await server.start();
 
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors());
 
-    // Important for MERN Setup: When our application runs from production, it functions slightly differently than in development
-    // In development, we run two servers concurrently that work together
-    // In production, our Node server runs and delivers our client-side bundle from the dist/ folder 
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/dist')));
+  // Define a new endpoint to serve the environment variable
+  app.get("/api/env-variable", (req, res) => {
+    // Assuming your environment variable is stored in process.env
+    const envVariable = process.env.WEATHER_API_KEY;
+    res.json({ envVariable });
+  });
 
-        app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-        });
-    };
+  // Important for MERN Setup: When our application runs from production, it functions slightly differently than in development
+  // In development, we run two servers concurrently that work together
+  // In production, our Node server runs and delivers our client-side bundle from the dist/ folder
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    // Important for MERN Setup: Any client-side requests that begin with '/graphql' will be handled by our Apollo Server
-    app.use('/graphql', expressMiddleware(server));
-
-    // Define a new endpoint to serve the environment variable
-    app.get('/api/env-variable', (req, res) => {
-        // Assuming your environment variable is stored in process.env
-        const envVariable = process.env.WEATHER_API_KEY;
-        res.json({ envVariable });
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
+  }
 
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}!`);
-            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-        });
+  // Important for MERN Setup: Any client-side requests that begin with '/graphql' will be handled by our Apollo Server
+  app.use("/graphql", expressMiddleware(server));
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
+  });
 };
 
 startApolloServer();
