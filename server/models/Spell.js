fc2662a7845431f15623bd0067004c912acce869
unknown <anthony_rosati@yahoo.com>
2024-05-08 21:29:55 -0500

    Several spells added to seeds

--- server/models/Spell.js
+++ server/models/Spell.js
@@ -22,6 +22,12 @@ const spellSchema = new Schema({
     type: String,
     required: true,
   },
+  effectsArray: {
+    type: Array,
+  },
+  atHigherLevel: {
+    type: String,
+  },
   components: {
     type: String,
     required: true,
@@ -30,6 +36,34 @@ const spellSchema = new Schema({
     type: Array,
     required: true,
   },
+  isConcentration: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  classList: {
+    type: Array,
+    required: true,
+  },
+  sourceBook: {
+    type: String,
+    required: true,
+    default: "Player's Handbook",
+  },
+  castingTime: {
+    type: String,
+    required: true,
+    default: "One Action",
+  },
+  duration: {
+    type: String,
+    required: true,
+    default: "Instantaneous"
+  },
+  range: {
+    type: String,
+    required: true,
+  }
 });
 
 const Spell = model("Spell", spellSchema);
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const Location = require("./Location");
 const User = require("./User");
 const FantasyLocation = require("./FantasyLocation");
+const Spell = require("./Spell");
 
-module.exports = { User, Location, FantasyLocation };
+module.exports = { User, Location, FantasyLocation, Spell };
