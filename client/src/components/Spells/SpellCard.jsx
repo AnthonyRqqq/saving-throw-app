d28308ee944a7ea9308628fa1e42f9c3ea9f741c
unknown <anthony_rosati@yahoo.com>
2024-05-24 20:16:21 -0500

    Structure of spell card complete

--- client/src/components/Spells/SpellCard.jsx
+++ client/src/components/Spells/SpellCard.jsx
@@ -1,43 +1,87 @@
-import { Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
 
-export default function SpellCard (spell){
-    return (
-        <Container>
-            <Row>
-                <Col>---SPELL TITLE GOES HERE---</Col>
-            </Row>
+export default function SpellCard(spell) {
+  let spellLevelText;
+  switch (spell.level) {
+    case 0:
+      spellLevelText = "Cantrip";
+      break;
+    case 1:
+      spellLevelText = "1st-Level";
+      break;
+    case 2:
+      spellLevelText = "2nd-Level";
+      break;
+    case 3:
+      spellLevelText = "3rd-Level";
+      break;
+    case 4:
+      spellLevelText = "4th-Level";
+      break;
+    case 5:
+      spellLevelText = "5th-Level";
+      break;
+    case 6:
+      spellLevelText = "6th-Level";
+      break;
+    case 7:
+      spellLevelText = "7th-Level";
+      break;
+    case 8:
+      spellLevelText = "8th-Level";
+      break;
+    case 9:
+      spellLevelText = "9th-Level";
+  }
 
-            <Row>
-                <Col>SOURCE</Col>
-            </Row>
-            <Row>
-                <Col>SCHOOL --- LEVEL</Col>
-            </Row>
+  const spellListString = spell.spellList.join(", ");
 
-            <Row>
-                <Col>CASTING TIME</Col>
-            </Row>
-            <Row>
-                <Col>RANGE</Col>
-            </Row>
-            <Row>
-                <Col>COMPONENTS</Col>
-            </Row>
-            <Row>
-                <Col>DURATION</Col>
-            </Row>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>{spell.name}</h2>
+        </Col>
+      </Row>
 
-            <Row>
-                <Col>DESCRIPTION</Col>
-            </Row>
+      <Row>
+        <Col>Source: {spell.sourceBook}</Col>
+      </Row>
+      <Row>
+        <Col>
+          {spell.level === 0
+            ? `${spell.school} ${spellLevelText}`
+            : `${spellLevelText} ${spell.school}`}
+        </Col>
+      </Row>
 
-            <Row>
-                <Col>AT HIGHER LEVELS</Col>
-            </Row>
+      <Row>
+        <Col>Casting Time: {spell.castingTime}</Col>
+      </Row>
+      <Row>
+        <Col>Range: {spell.range}</Col>
+      </Row>
+      <Row>
+        <Col>
+          Components: {spell.components}{" "}
+          {spell.materialComponents ? `(${spell.materialComponents})` : ""}
+        </Col>
+      </Row>
+      <Row>
+        <Col>Duration: {spell.duration}</Col>
+      </Row>
 
-            <Row>
-                <Col>Spell lists</Col>
-            </Row>
-        </Container>
-    )
+      <Row>
+        <Col>{spell.description}</Col>
+      </Row>
+
+      <Row>
+        <Col>At Higher Levels: {spell.atHigherLevel}</Col>
+      </Row>
+
+      <Row>
+        <Col>Spell Lists: {spellListString}</Col>
+      </Row>
+    </Container>
+  );
 }
