6fe1da1d797c71b39294f4ecd257ac7357881f23
unknown <anthony_rosati@yahoo.com>
2024-11-26 19:42:10 -0600

    Initial condition dropdown

--- client/src/components/Conditions/Conditions.jsx
+++ client/src/components/Conditions/Conditions.jsx
@@ -1,18 +1,30 @@
 import { conditions } from "./conditions";
+import "./conditions.css";
 
 export default function Conditions() {
   return (
-    <div>
+    <div className="accordion">
       {Object.keys(conditions).map((condition) => {
         return (
-          <div key={condition}>
-            <h3>{condition}</h3>
-            <ul>
-              {conditions[condition].effects &&
-                conditions[condition].effects.map((effect, index) => {
-                  return <li key={index}>{effect}</li>;
-                })}
-            </ul>
+          <div className="accordion-item" key={condition}>
+            <span
+              className="accordion-button accordion-header"
+              data-bs-toggle="collapse"
+              data-bs-target={`#panel-${condition}`}
+            >
+              {condition}
+            </span>
+            <div
+              id={`panel-${condition}`}
+              className="accordion-collapse collapse"
+            >
+              <ul className="accordion-body">
+                {conditions[condition].effects &&
+                  conditions[condition].effects.map((effect, index) => {
+                    return <li key={index}>{effect}</li>;
+                  })}
+              </ul>
+            </div>
           </div>
         );
       })}
