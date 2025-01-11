26b8d46f5e5ef974598e3fa6420683c13b74291f
Anthony Rosati <anthony_rosati@yahoo.com>
2025-01-10 20:52:31 -0600

    Fix type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,62 +1,130 @@
 const typeDefs = `
 
-  # Type for authentication
-  type Auth {
-    token: ID!
-    user: User
-  }
+# Type for authentication
+type Auth {
+  token: ID!
+  user: User
+}
 
-  # Type for user
-  type User {
-    _id: ID
-    email: String!
-    fantasyLocations: [FantasyLocation]
-    weatherCreateInstruction: Boolean
-  }
+# Type for user
+type User {
+  _id: ID
+  email: String!
+  fantasyLocations: [FantasyLocation]
+  weatherCreateInstruction: Boolean
+}
 
-  # Type for locations used in weather API
-  type Location {
-    _id: ID
-    name: String
-    lat: Float!
-    lon: Float!
-    tags: [String!]
-  }
+# Type for locations used in weather API
+type Location {
+  _id: ID
+  name: String
+  lat: Float!
+  lon: Float!
+  tags: [String!]
+}
 
-  # Type for tag inputs
-  type TagInput {
-    tags: [String!]
-  }
+# Type for tag inputs
+type TagInput {
+  tags: [String!]
+}
 
-  # Type for fantasy locations
-  type FantasyLocation {
-    _id: ID
-    name: String!
-    realLocation: Location!
-  }
+# Type for fantasy locations
+type FantasyLocation {
+  _id: ID
+  name: String!
+  realLocation: Location!
+}
 
-  # Type for spells
-  type Spell {
-    _id: ID,
-    name: String!,
-    level: Int!,
-    school: String!,
-    isRitual: Boolean,
-    description: String!,
-    effectsArray: [String],
-    atHigherLevel: String,
-    components: String!,
-    materialComponents: [String],
-    isConcentration: Boolean,
-    classList: [String!],
-    sourceBook: String,
-    castingTime: String,
-    duration: String,
-    range: String!,
-    createdBy: User,
-    statBlock: [StatBlock],
-    table: [String]
-  }
+type Table {
+  header: String!
+  details: [String]
+}
+
+# Type for spells
+type Spell {
+  _id: ID
+  name: String!
+  level: Int!
+  school: String!
+  isRitual: Boolean
+  description: String!
+  effectsArray: [String]
+  atHigherLevel: String
+  components: String!
+  materialComponents: [String]
+  isConcentration: Boolean
+  classList: [String!]
+  sourceBook: String
+  castingTime: String
+  duration: String
+  range: String!
+  createdBy: User
+  statBlock: [StatBlock]
+  table: [Table]
+}
+
+type Trait {
+  title: String!
+  description: String!
+}
+
+type Action {
+  title: String!
+  type: String
+  hitBonus: String
+  range: String
+  target: String
+  description: String!
+}
+
+type Reaction {
+  title: String!
+  type: String
+  hitBonus: String
+  range: String
+  target: String
+  description: String!
+}
+
+type BonusAction {
+  title: String!
+  type: String
+  hitBonus: String
+  range: String
+  target: String
+  description: String!
+}
+
+# StatBlock for spell or creature
+type StatBlock {
+  _id: ID
+  name: String!
+  size: String!
+  type: String!
+  alignment: String
+  armorClass: String!
+  hitPoints: String!
+  speed: String!
+  strength: Int!
+  dexterity: Int!
+  constitution: Int!
+  intelligence: Int!
+  wisdom: Int!
+  charisma: Int!
+  conditionImmunities: [String]
+  damageImmunities: [String]
+  resistances: [String]
+  skills: [String]
+  sense: [String!]
+  language: [String]
+  challenge: String
+  proficiency: String
+  trait: [Trait]
+  action: [Action]
+  reaction: [Reaction]
+  bonusAction: [BonusAction]
+}
+
 
   type Query {
     users: [User]
