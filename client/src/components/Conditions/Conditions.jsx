6e60d3939be89c03f9d73da0883ebf8c814ff976
Anthony Rosati <anthony_rosati@yahoo.com>
2025-01-25 23:26:37 -0600

    Functional conditions list

--- client/src/components/Conditions/Conditions.jsx
+++ client/src/components/Conditions/Conditions.jsx
@@ -1,33 +1,37 @@
 import { conditions } from "./conditions";
+import { Col } from "react-bootstrap";
 import "./conditions.css";
 
 export default function Conditions() {
   return (
-    <div className="accordion">
+    <div style={{ display: 'flex', justifyContent: 'center'}}>
+    <div className="accordion accordion-flush">
       {Object.keys(conditions).map((condition) => {
         return (
           <div className="accordion-item" key={condition}>
-            <span
-              className="accordion-button accordion-header"
+            <h2
+              className="ps-0 fw-bold fs-5 accordion-button accordion-header"
               data-bs-toggle="collapse"
               data-bs-target={`#panel-${condition}`}
+              style={{ color: 'black'}}
             >
               {condition}
-            </span>
+            </h2>
             <div
               id={`panel-${condition}`}
               className="accordion-collapse collapse"
             >
-              <ul className="accordion-body">
+              <ul className="accordion-body mb-0">
                 {conditions[condition].effects &&
                   conditions[condition].effects.map((effect, index) => {
-                    return <li key={index}>{effect}</li>;
+                    return <li className="py-1" key={index}>{effect}</li>;
                   })}
               </ul>
             </div>
           </div>
         );
       })}
+    </div>
     </div>
   );
 }
