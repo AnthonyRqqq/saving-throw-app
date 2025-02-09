90bdffb9ced90fd54d332a74f5948de4939a97b1
Anthony Rosati <anthony_rosati@yahoo.com>
2025-02-08 19:44:20 -0600

    Spell list added to index for models

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,13 @@ const User = require("./User");
 const FantasyLocation = require("./FantasyLocation");
 const Spell = require("./Spell");
 const StatBlock = require("./StatBlock");
+const SpellList = require("./SpellList");
 
-module.exports = { User, Location, FantasyLocation, Spell, StatBlock };
+module.exports = {
+  User,
+  Location,
+  FantasyLocation,
+  Spell,
+  StatBlock,
+  SpellList,
+};
