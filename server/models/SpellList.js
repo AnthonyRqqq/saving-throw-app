1308b7f5a2dbadc41700f54d923227265207bf25
Anthony Rosati <anthony_rosati@yahoo.com>
2025-02-08 19:43:47 -0600

    Spell list schema added

---
+++ server/models/SpellList.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const spellListSchema = new Schema({
+  spell: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Spell",
+    },
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  spellSlots: [
+    {
+      level: {
+        type: String,
+        required: true,
+      },
+      expended: {
+        type: Number,
+        required: true,
+      },
+      available: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  preparedSpells: [
+    {
+      type: String,
+    },
+  ],
+  class: {
+    type: String,
+    required: true,
+  },
+});
+
+const SpellList = model("SpellList", spellListSchema);
+
+module.exports = SpellList;
