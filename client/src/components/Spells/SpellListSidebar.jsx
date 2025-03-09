911dc383ba5ea5706790efeed892d160fcc7e869
Anthony Rosati <anthony_rosati@yahoo.com>
2025-03-08 20:15:20 -0600

    Buttons added to spell list sidebar

--- client/src/components/Spells/SpellListSidebar.jsx
+++ client/src/components/Spells/SpellListSidebar.jsx
@@ -1,16 +1,29 @@
 import "./SpellListSidebar.css";
 
-export default function SpellListSidebar({ list, allLists, setListDisplay }) {
+export default function SpellListSidebar({
+  list,
+  allLists,
+  setListDisplay,
+  viewAllSpells,
+  reloadList,
+}) {
   const handleListChange = (e) => {
     const newList = e.target.value;
     if (newList === list.name) return;
 
     const selectedList = allLists.find((list) => list.name === newList);
     setListDisplay(selectedList);
+    reloadList();
   };
 
   return (
     <div className="list-sidebar-el">
+      <div>
+        <button onClick={() => viewAllSpells()}>Add Spells</button>
+        <button onClick={() => setListDisplay(null)}>View All Lists</button>
+        <button onClick={() => removeSpells()}>Remove Spells</button>
+      </div>
+
       <select defaultValue={list.name} onChange={handleListChange}>
         {allLists.map((listItem, index) => (
           <option key={index} value={listItem.name}>
