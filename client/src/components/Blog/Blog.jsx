b1b3b1576871a5d4b07705bbfb9b3d72c6ad7085
Anthony Rosati <anthony_rosati@yahoo.com>
2025-04-01 21:26:55 -0500

    Blog css start

--- client/src/components/Blog/Blog.jsx
+++ client/src/components/Blog/Blog.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client";
 import { GET_BLOG_POSTS } from "../../utils/queries";
 import { useEffect } from "react";
 import { Spinner } from "react-bootstrap";
+import './Blog.css'
 
 export default function Blog() {
   const { data, loading } = useQuery(GET_BLOG_POSTS);
@@ -18,19 +19,19 @@ export default function Blog() {
     );
   }
   return (
-    <div>
+    <div className="blog">
       {data.blogPosts.map((post) => (
         <div key={post._id}>
-          <h4>{post.title}</h4>
-          <div>{post.body}</div>
-          <div>
+          <h4 className="blogHeader">{post.title}</h4>
+          <div className="blogBody">{post.body}</div>
+          <div className="blogNotes">
             <ul>
               {post.notes.map((note) => (
                 <li key={note}>{note}</li>
               ))}
             </ul>
           </div>
-          <div>
+          <div className="blogDate">
             {new Date(post.date).toLocaleString("en-US", {
               year: "numeric",
               month: "long",
