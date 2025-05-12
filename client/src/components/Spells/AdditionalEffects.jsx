4f08b17fd95d0a66432678209f730c4f402d1ed2
Anthony Rosati <anthony_rosati@yahoo.com>
2025-05-11 20:30:46 -0500

    clean up additionaleffects.jsx

--- client/src/components/Spells/AdditionalEffects.jsx
+++ client/src/components/Spells/AdditionalEffects.jsx
@@ -2,17 +2,15 @@ import { Row, Col } from "react-bootstrap";
 
 // Handles iterating through and rendering each item of an effects array for a spell card
 export default function AdditionalEffects({ effectsArray }) {
-  const renderEffectsArray = (effectsArray) => {
-    return effectsArray.map((effect, index) => (
-      <div className="spellCardField pb-3" key={index}>
-        {effect}
-      </div>
-    ));
-  };
-
   return (
     <Row className="spellDescription pt-0 pb-4">
-      <Col>{renderEffectsArray(effectsArray)}</Col>
+      <Col>
+        {effectsArray.map((effect, index) => (
+          <div className="spellCardField pb-3" key={index}>
+            {effect}
+          </div>
+        ))}
+      </Col>
     </Row>
   );
 }
